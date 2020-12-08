76351301c9fc307fca8719bb22c2c59c1c4a25d4
Fraser Mckenzie <swissfraser@Air20.local>
2020-12-08 13:52:36 +0000

    first commit

--- src/modules/date-helper-functions.ts
+++ src/modules/date-helper-functions.ts
@@ -11,7 +11,7 @@ const millisecondsInOneDay = 24 * 60 * 60 * 1000;
 export const elapsedDays = (startDate: Date, endDate: Date): number => {
 
     // strip the time from the dates
-    let start = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate());
-    let end = new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate());
+    const start = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate());
+    const end = new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate());
     return Math.round(Math.abs((end.getTime() - start.getTime()) / millisecondsInOneDay));
 }
